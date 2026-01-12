e577382213a79cef73acd0ce8fc4b0f6787a3f86
Alejandro <alejandro.perez.paniagua1@gmail.com>
2026-01-12 20:07:57 +0100

    Feat: Create tasks migration

---
+++ database/migrations/2026_01_12_185103_create_tasks_table.php
@@ -0,0 +1,34 @@
+<?php
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    public function up(): void
+    {
+        Schema::create('tasks', function (Blueprint $table) {
+            $table->id();
+
+            $table->foreignId('user_id')
+                ->constrained()
+                ->cascadeOnDelete();
+
+            $table->string('title', 150);
+            $table->text('content')->nullable();
+
+            $table->timestamp('due_date')->nullable();
+            $table->timestamp('completed_at')->nullable();
+
+            $table->timestamps();
+
+            // Allows task recovery and preserves historical data for auditing and logs
+            $table->softDeletes();
+        });
+    }
+
+    public function down(): void
+    {
+        Schema::dropIfExists('tasks');
+    }
+};
